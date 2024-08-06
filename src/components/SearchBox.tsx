1871ac7eff4e5a4b9fd19c89b48d57ad553d657d
Teaflavored <auster.s.chen@gmail.com>
2024-08-06 15:49:55 -0700

    switch to using useQuery

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -11,38 +11,23 @@ import {
 } from "@/components/ui/command";
 import { api } from "@/lib/client/api";
 import { useDebouncedValue } from "@mantine/hooks";
+import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const DEBOUNCE_TIMEOUT = 200;
 
 export const SearchBox = () => {
   const [value, setValue] = useState("");
   const [debouncedValue] = useDebouncedValue(value, DEBOUNCE_TIMEOUT);
-  const [results, setResults] = useState<GetResponse["results"]>([]);
-  const [hasFetched, setHasFetched] = useState(false);
-  const [isFetching, setIsFetching] = useState(false);
   const router = useRouter();
 
-  useEffect(() => {
-    const fetch = async () => {
-      setIsFetching(true);
-      try {
-        const data = await api.get<GetResponse>(
-          `/search?query=${debouncedValue}`
-        );
-        setHasFetched(true);
-
-        setResults(data.results);
-      } catch (e) {
-        console.error(e);
-      } finally {
-        setIsFetching(false);
-      }
-    };
-
-    fetch();
-  }, [debouncedValue]);
+  const { isFetched, isFetching, data } = useQuery({
+    queryKey: ["search", debouncedValue],
+    queryFn: async () => {
+      return await api.get<GetResponse>(`/search?query=${debouncedValue}`);
+    },
+  });
 
   return (
     <Command>
@@ -54,13 +39,13 @@ export const SearchBox = () => {
         }}
       />
       <CommandList>
-        {!isFetching && results.length === 0 && hasFetched && (
+        {!isFetching && data?.results.length === 0 && isFetched && (
           <CommandEmpty>No results found.</CommandEmpty>
         )}
         {isFetching && <CommandEmpty>Loading...</CommandEmpty>}
-        {!!results.length && (
+        {!!data?.results.length && (
           <CommandGroup heading="Collections">
-            {results.map((result) => (
+            {(data.results || []).map((result) => (
               <CommandItem
                 key={result.collectionSlug}
                 onSelect={() => {
